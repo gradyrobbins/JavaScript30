34653fa1be3d53dc1d1db7cc8466396fe33ace4b
Grady Robbins <gradyrobbins13@gmail.com>
2018-10-04 12:24:25 -0500

    .key.classList.add(CSS class)

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -3,5 +3,11 @@ console.log("grady")
 // add event listeners
 window.addEventListener('keydown', function(e){
     const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return //stops the function if keydown doesn't have corresponding data-key"
     console.log(audio)
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add('playing');
+    console.log(key)
 })
