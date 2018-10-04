9bbd9d3c10fcd60a61b31564561d55444473e326
Grady Robbins <gradyrobbins13@gmail.com>
2018-10-04 12:13:24 -0500

    event listener captures data attribute + cons.log(e.keyCode)

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -2,5 +2,6 @@ console.log("grady")
 
 // add event listeners
 window.addEventListener('keydown', function(e){
-    console.log(e)
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(audio)
 })
